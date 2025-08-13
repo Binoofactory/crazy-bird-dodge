af9b83aaec075a3714ce82ba95c06bd473c5aef8
minwoojeon <botbinoo@naver.com>
2025-08-13 15:17:59 +0900

    fix: removed expo files

--- crazy-bird-dodge/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'crazy-bird-dodge',
-  webDir: 'dist'
-};
-
-export default config;
